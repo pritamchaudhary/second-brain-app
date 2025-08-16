aa5eccde5387f55ccf433647295821c5ee73b637
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2025-08-17 00:49:39 +0530

    add signup and  signin functionality

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,53 @@
 
 import express from 'express';
 import jwt from 'jsonwebtoken'
+import { UserModel } from './db';
+
+const JWT_PASSWORD = "123123"
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async(req, res) => {
+    // TODO: zod validation, hash the password
+    const username = req.body.username;
+    const password = req.body.password;
 
+    try {
+       await UserModel.create({
+            username: username,
+            password: password
+        })
+
+        res.json({
+            message: "User signed up"
+        }) 
+    } catch (e) {
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
 })
 
-app.post("/api/v1/signin", (req, res) => {
-
+app.post("/api/v1/signin", async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if(existingUser){
+        const token = jwt.sign({
+            id: existingUser._id,
+        }, JWT_PASSWORD)
+        res.json({
+            token
+        })
+    } else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 })
 
 app.get("/api/v1/content", (req, res) => {
@@ -27,3 +65,5 @@ app.post("/api/v1/brain/share", (req, res) => {
 app.get("/api/v1/brain/:shareLink", (req, res) => {
 
 })
+
+app.listen(3000);
